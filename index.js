65eade87a24259487ce0ffa08c2f3d339ebd4e08
08Uzer <121750611+08Uzair@users.noreply.github.com>
2024-08-27 13:43:12 +0530

    last commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// app.js
 import express from "express";
+import cors from "cors";
 import { userRouter } from "./routes/Auth.js";
+
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/v1/users", userRouter);
 
 app.listen(8900, () => {
   console.log("Server is running on port 8900");
-
 });
